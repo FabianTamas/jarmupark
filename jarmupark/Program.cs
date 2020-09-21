48f29c53496912c3ed2ee6cd15d557257f527922
FabianTamas <fabiant0130@gmail.com>
2020-09-21 13:04:03 +0200

    Működő autó osztály

--- jarmupark/Program.cs
+++ jarmupark/Program.cs
@@ -10,6 +10,30 @@ namespace jarmupark
     {
         static void Main(string[] args)
         {
+            Auto lada = new Auto("piros", "Lada 2112", 11);
+            Auto trabant = new Auto("zöld", "Trabant 1.1", 8);
+
+            lada.Tankol(20);
+            trabant.Tankol(20);
+
+            lada.Megy(0);
+            trabant.Megy(0);
+
+            lada.Allapot();
+            trabant.Allapot();
+
+            Console.Write("Mennyit megy a lada? ");
+            int km = int.Parse(Console.ReadLine());
+            lada.Megy(km);
+            lada.Allapot();
+
+            Console.Write("Mennyit megy a trabant? ");
+            int km1 = int.Parse(Console.ReadLine());
+            lada.Megy(km1);
+            lada.Allapot();
+
+
+            Console.ReadKey();
         }
     }
 }
